c1f51b089da51c0917cc5e519a981a6fa37c9e77
Mizanur Rahman <mizanur511.cse@gmail.com>
2026-04-14 01:14:11 +0600

    mizan

---
+++ Start.java
@@ -0,0 +1,10 @@
+public class Start {
+    public static void main(String[] args) {
+        AdvancedCalculator a1 = new AdvancedCalculator();
+        a1.add(20, 5);
+        a1.subtract(20,5);
+        a1.multiplication(20,5);
+        a1.divition(20, 5);
+    }
+    
+}
