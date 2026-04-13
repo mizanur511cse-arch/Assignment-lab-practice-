a911a3fb36710081d06eb098e9b15f79c96de8a0
Mizanur Rahman <mizanur511.cse@gmail.com>
2026-04-14 01:16:59 +0600

    Add files via upload

---
+++ AdvancedCalculator.java
@@ -0,0 +1,11 @@
+public class AdvancedCalculator extends Calculator{
+    int n1;
+    int n2;
+    void multiplication(int n1, int n2){
+        System.out.println("Multiplication="+(n1*n2));
+    }
+    void division(int n1, int n2){
+        System.out.println("Division="+(n1/n2));
+    }
+    
+}
---
+++ Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+    int n1;
+    int n2;
+    void add(int n1, int n2){
+        System.out.println("Addition="+(n1+n2));
+    }
+      void subtract(int n1, int n2){
+        System.out.println("Substraction="+(n1-n2));
+     }
+}
